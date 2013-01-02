e958429ce273160f76193d877d3da288c491468a
grimrose <grwin7@gmail.com>
2013-01-03 00:28:14 +0900

    Theories end

---
+++ src/test/groovy/chapter5/CalcTheoriesTest.groovy
@@ -0,0 +1,30 @@
+package chapter5
+
+import org.hamcrest.CoreMatchers
+import org.junit.experimental.theories.DataPoints
+import org.junit.experimental.theories.Theories
+import org.junit.experimental.theories.Theory
+import org.junit.runner.RunWith
+
+import static org.junit.Assert.assertThat
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: grimrose
+ * Date: 2013/01/03
+ * Time: 0:01
+ * To change this template use File | Settings | File Templates.
+ */
+@RunWith(Theories)
+class CalcTheoriesTest {
+
+    @DataPoints
+    public static int[][] VALUES = [[0, 0, 0], [0, 1, 1], [1, 0, 1], [3, 4, 7]] as int[][]
+
+    @Theory
+    void add(int[] values) {
+        Calc sut = new Calc()
+        assertThat(sut.add(values[0], values[1]), CoreMatchers.is(values[2]))
+    }
+
+}
