db551545c97532561dd7ef0a9428dac4b15b8449
grimrose <grwin7@gmail.com>
2013-01-12 17:28:13 +0900

    update VerifierExampleTest

--- src/test/groovy/chapter9/VerifierExampleTest.groovy
+++ src/test/groovy/chapter9/VerifierExampleTest.groovy
@@ -1,11 +1,66 @@
 package chapter9
 
-/**
- * Created with IntelliJ IDEA.
- * User: grimrose
- * Date: 2013/01/12
- * Time: 14:12
- * To change this template use File | Settings | File Templates.
- */
+import org.junit.After
+
+import static org.hamcrest.CoreMatchers.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.Verifier
+
+import static org.junit.Assert.assertThat
+
 class VerifierExampleTest {
+
+    @Rule
+    public Verifier verifier = new Verifier() {
+
+        @Override
+        protected void verify() throws Throwable {
+            assertThat("value should be 0.", sut.value, is(0))
+        }
+    };
+    VerifierExample sut;
+
+    @Before
+    void setUp() {
+        // Setup
+        sut = new VerifierExample()
+    }
+
+    @After
+    void tearDown() {
+        sut.set 0
+    }
+
+    @Test
+    void "getValueで計算結果を取得する"() {
+        // Setup
+        sut.set 200
+        sut.add(100)
+        sut.minus(50)
+        // Exercise
+        int actual = sut.value
+        // Verify
+        assertThat actual, is(250)
+    }
+
+    static class VerifierExample {
+
+        int value
+
+        void set(int i) {
+            value = i
+        }
+
+        void add(int i) {
+            value += i
+        }
+
+        void minus(int i) {
+            value -= i
+        }
+
+    }
+
 }
